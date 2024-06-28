828a18607e5b1270f9c963729a83f85844064700
itc-s23018 <s23018@std.it-college.ac.jp>
2024-06-28 02:39:27 +0000

    d49Q6kadai

---
+++ d49/d49Q6kadai01.java
@@ -0,0 +1,21 @@
+/* d49Q6kadai01
+s23018
+StringBuilderクラスを使って与えられた引数を連結し、表示する課題
+実行の際は引数を受けるよう設計
+ 【実行例】
+ > java d49Q6kadai01 IT カレッジ 沖縄
+ITカレッジ沖縄
+*/
+
+import java.lang.StringBuilder;
+
+public class d49Q6kadai01 {
+	public static void main(String[] args) {
+	 	StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < args.length; i++) {
+			sb.append(args[i]);
+
+		}
+		System.out.println(sb.toString());
+	}
+}
