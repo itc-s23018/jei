de34479ad082abc9fcd0e4fe974193d09f072a48
itc-s23018 <s23018@std.it-college.ac.jp>
2024-07-26 00:54:26 +0000

    d51Q26kadai

---
+++ d51/d51Q26kadai.java
@@ -0,0 +1,12 @@
+public class d51Q26kadai {
+	public static void main(String[] args) {
+		int n = 1;
+		String s = "";
+		switch (n) {
+			case 1 -> s += "one";
+			case 2 -> s += "two";
+			default -> s += "?";
+		}
+		System.out.println(s);
+	}
+}
